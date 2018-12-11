74610da8bba2352c34f82407bf57d9d0ee56ddef
Je Min Son <jeminson@Jeui-MBP.attlocal.net>
2018-12-10 22:28:27 -0600

    init

---
+++ MVVM_WeatherWithFiveDays/MVVM_WeatherWithFiveDays/ViewController/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_WeatherWithFiveDays
+//
+//  Created by Je Min Son on 12/10/18.
+//  Copyright © 2018 Jason Son. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MVVM_WeatherWithFiveDays/Pods/Target Support Files/Pods-MVVM_WeatherWithFiveDays/Pods-MVVM_WeatherWithFiveDays-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVVM_WeatherWithFiveDays : NSObject
+@end
+@implementation PodsDummy_Pods_MVVM_WeatherWithFiveDays
+@end
