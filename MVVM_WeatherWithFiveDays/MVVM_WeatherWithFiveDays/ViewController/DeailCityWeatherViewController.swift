cbef1fdbafeb75b70de4dd421b2594efbae77d30
Je Min Son <jeminson@Jeui-MacBook-Pro.local>
2018-12-12 16:59:26 -0600

    halfway done with Detail VC

--- MVVM_WeatherWithFiveDays/MVVM_WeatherWithFiveDays/ViewController/DeailCityWeatherViewController.swift
+++ MVVM_WeatherWithFiveDays/MVVM_WeatherWithFiveDays/ViewController/DeailCityWeatherViewController.swift
@@ -7,17 +7,50 @@
 //
 
 import UIKit
+import Kingfisher
 
 class DetailCityWeatherViewController: UIViewController {
 
+    @IBOutlet weak var currentWeatherIconImgView: UIImageView!
+    @IBOutlet weak var selectedCityImgView: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var currentWeatherTempLabel: UILabel!
+    @IBOutlet weak var currentWeatherWindSpeedLabel: UILabel!
+    
+    
     let cityViewModel = CityViewModel()
+    let weatherViewModel = WeatherViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = cityViewModel.allCityModels[0].cityName
+     
+        getSelectedCityWeahter()
+        getCurrentCityByName(currentCityName: cityViewModel.allCityModels[0].cityName!)
+    }
+}
 
+extension DetailCityWeatherViewController {
+    func getCurrentCityByName(currentCityName: String) {
+        cityViewModel.getCityImage(cityName: currentCityName) { (cityImgUrl) in
+            DispatchQueue.main.async {
+                self.selectedCityImgView.kf.setImage(with: cityImgUrl as? Resource)
+            }
+        }
     }
     
-
+    func getSelectedCityWeahter() {
+        weatherViewModel.getCurrentCityWeatherByCoordinate(lat: cityViewModel.allCityModels[0].latitude!, long: cityViewModel.allCityModels[0].longitude!) { (weatherModel) in
+            
+            DispatchQueue.main.async {
+                self.currentWeatherLabel.text = self.weatherViewModel.weatherInfo[0].weatherMain!
+                self.currentWeatherTempLabel.text = String(self.weatherViewModel.weatherInfo[0].temp!)
+                self.currentWeatherWindSpeedLabel.text = String(self.weatherViewModel.weatherInfo[0].windSpeed!)
+                self.currentWeatherIconImgView.kf.setImage(with: self.weatherViewModel.weatherInfo[0].weatherIcon)
+            }
+            
+        }
+        
+    }
 }
