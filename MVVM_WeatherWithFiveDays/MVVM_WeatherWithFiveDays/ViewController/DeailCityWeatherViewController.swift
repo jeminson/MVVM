87cb5e393f8dd0b54ed780089b1d6e026dc402f8
Je Min Son <jeminson@Jeui-MacBook-Pro.local>
2018-12-12 23:05:30 -0600

    done

--- MVVM_WeatherWithFiveDays/MVVM_WeatherWithFiveDays/ViewController/DeailCityWeatherViewController.swift
+++ MVVM_WeatherWithFiveDays/MVVM_WeatherWithFiveDays/ViewController/DeailCityWeatherViewController.swift
@@ -17,6 +17,25 @@ class DetailCityWeatherViewController: UIViewController {
     @IBOutlet weak var currentWeatherTempLabel: UILabel!
     @IBOutlet weak var currentWeatherWindSpeedLabel: UILabel!
     
+    @IBOutlet weak var secondDayTimeLabel: UILabel!
+    @IBOutlet weak var secondDayWeatherIconImgView: UIImageView!
+    @IBOutlet weak var secondDayWeatherLabel: UILabel!
+    
+    @IBOutlet weak var thirdDayTimeLabel: UILabel!
+    @IBOutlet weak var thirdDayWeatherIconImgView: UIImageView!
+    @IBOutlet weak var thirdDayWeatherLabel: UILabel!
+    
+    
+    @IBOutlet weak var fourthDayTimeLabel: UILabel!
+    @IBOutlet weak var fourthDayWeatherIconImgView: UIImageView!
+    @IBOutlet weak var fourthDayWeatherLabel: UILabel!
+    
+    
+    @IBOutlet weak var fifthDayTimeLabel: UILabel!
+    @IBOutlet weak var fifthDayWeatherIconImgView: UIImageView!
+    @IBOutlet weak var fifthDayWeatherLabel: UILabel!
+    
+    
     
     let cityViewModel = CityViewModel()
     let weatherViewModel = WeatherViewModel()
@@ -48,6 +67,22 @@ extension DetailCityWeatherViewController {
                 self.currentWeatherTempLabel.text = String(self.weatherViewModel.weatherInfo[0].temp!)
                 self.currentWeatherWindSpeedLabel.text = String(self.weatherViewModel.weatherInfo[0].windSpeed!)
                 self.currentWeatherIconImgView.kf.setImage(with: self.weatherViewModel.weatherInfo[0].weatherIcon)
+                
+                self.secondDayWeatherLabel.text = self.weatherViewModel.weatherInfo[8].weatherMain!
+                self.secondDayTimeLabel.text = String(self.weatherViewModel.weatherInfo[8].time!)
+                self.secondDayWeatherIconImgView.kf.setImage(with: self.weatherViewModel.weatherInfo[8].weatherIcon)
+                
+                self.thirdDayWeatherLabel.text = self.weatherViewModel.weatherInfo[16].weatherMain!
+                self.thirdDayTimeLabel.text = String(self.weatherViewModel.weatherInfo[16].time!)
+                self.thirdDayWeatherIconImgView.kf.setImage(with: self.weatherViewModel.weatherInfo[16].weatherIcon)
+                
+                self.fourthDayWeatherLabel.text = self.weatherViewModel.weatherInfo[24].weatherMain!
+                self.fourthDayTimeLabel.text = String(self.weatherViewModel.weatherInfo[24].time!)
+                self.fourthDayWeatherIconImgView.kf.setImage(with: self.weatherViewModel.weatherInfo[24].weatherIcon)
+                
+                self.fifthDayWeatherLabel.text = self.weatherViewModel.weatherInfo[32].weatherMain!
+                self.fifthDayTimeLabel.text = String(self.weatherViewModel.weatherInfo[32].time!)
+                self.fifthDayWeatherIconImgView.kf.setImage(with: self.weatherViewModel.weatherInfo[32].weatherIcon)
             }
             
         }
